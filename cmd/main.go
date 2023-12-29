4d0102657920d94c7bb605e39616500f4e24ab19
sshawn <github.mlxup@simplelogin.com>
2023-12-29 22:23:41 +0800

    openai pkg

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/lsshawn/go-todo/views"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 func main() {
+	dotenvErr := godotenv.Load()
+	if dotenvErr != nil {
+		log.Fatal("Error loading .env file")
+	}
 	app := echo.New()
 
 	app.GET("/ping", func(c echo.Context) error {
@@ -15,12 +28,80 @@ func main() {
 	})
 
 	app.GET("/", func(c echo.Context) error {
+		// aiChat()
 		component := views.Index()
 		return component.Render(context.Background(), c.Response().Writer)
 	})
+
+	app.GET("/chat-sse", echo.WrapHandler(http.HandlerFunc(sseHandler)))
 
 	app.Static("/css", "css")
 	app.Static("/static", "static")
 
 	app.Logger.Fatal(app.Start(":1323"))
+}
+
+func aiChat() {
+	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+	ctx := context.Background()
+
+	req := openai.ChatCompletionRequest{
+		Model:     openai.GPT3Dot5Turbo,
+		MaxTokens: 20,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: "Hello world",
+			},
+		},
+		Stream: true,
+	}
+	stream, err := client.CreateChatCompletionStream(ctx, req)
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+	defer stream.Close()
+
+	fmt.Printf("Stream response: ")
+
+	for {
+		response, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Printf("\nStream finished")
+			return
+		}
+		if err != nil {
+			fmt.Printf("Stream error: %v\n", err)
+			return
+		}
+
+		fmt.Printf(response.Choices[0].Delta.Content)
+	}
+}
+
+func sseHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+
+	// Send a new message every second
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Fprintf(w, "data: %s\n\n", time.Now().Format("15:04:05"))
+			flusher.Flush()
+		case <-r.Context().Done():
+			return
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -8,9 +8,11 @@ require (
 )
 
 require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/sashabaranov/go-openai v1.17.9 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
