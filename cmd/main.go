e9790d89546c0aef4cf488691c5385234f8e6695
sshawn <github.mlxup@simplelogin.com>
2023-12-30 06:39:29 +0800

    assistant

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net/http"
 	"os"
 	"time"
 
@@ -29,16 +28,81 @@ func main() {
 
 	app.GET("/", func(c echo.Context) error {
 		// aiChat()
+		aiAssistant()
 		component := views.Index()
 		return component.Render(context.Background(), c.Response().Writer)
 	})
-
-	app.GET("/chat-sse", echo.WrapHandler(http.HandlerFunc(sseHandler)))
 
 	app.Static("/css", "css")
 	app.Static("/static", "static")
 
 	app.Logger.Fatal(app.Start(":1323"))
+}
+
+func checkRun(client *openai.Client, threadID, runID string) (*openai.Run, error) {
+	ctx := context.Background()
+	for {
+		run, err := client.RetrieveRun(ctx, threadID, runID)
+		if err != nil {
+			return nil, fmt.Errorf("Error retrieving run: %v\n", err)
+		}
+
+		switch run.Status {
+		case "completed":
+			fmt.Println("Run completed")
+			return &run, nil
+		case "expired":
+			fmt.Println("Run expired")
+			return &run, nil
+		case "requires_action":
+			// Handle required action here (not implemented in this example)
+			fmt.Println("Run requires action")
+			return &run, nil
+		default:
+			fmt.Println("Running...")
+			time.Sleep(3 * time.Second)
+		}
+	}
+}
+
+func aiAssistant() {
+	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+	ctx := context.Background()
+
+	req := openai.CreateThreadAndRunRequest{
+		RunRequest: openai.RunRequest{
+			AssistantID: "asst_L2KwWjgK0JXDdgixLwzKkJYm",
+		},
+		Thread: openai.ThreadRequest{
+			Messages: []openai.ThreadMessage{
+				{
+					Role:    openai.ThreadMessageRoleUser,
+					Content: "write a code example for rust hello world",
+				},
+			},
+		},
+	}
+
+	resp, err := client.CreateThreadAndRun(ctx, req)
+	if err != nil {
+		fmt.Printf("CreateThreadAndRunRequest error: %v\n", err)
+		return
+	}
+	fmt.Printf("Thread ID: %s\n", resp.ThreadID)
+	fmt.Printf("Run ID: %s\n", resp.ID)
+
+	run, runErr := checkRun(client, resp.ThreadID, resp.ID)
+	if runErr != nil {
+		fmt.Printf("checkRun error: %v\n", runErr)
+		return
+	}
+	fmt.Printf("Run: %+v\n", run)
+
+	limit := 2
+	res, _ := client.ListMessage(ctx, resp.ThreadID, &limit, nil, nil, nil)
+	for _, msg := range res.Messages {
+		fmt.Printf("Message: %s\n", msg.Content[0].Text.Value)
+	}
 }
 
 func aiChat() {
@@ -77,31 +141,5 @@ func aiChat() {
 		}
 
 		fmt.Printf(response.Choices[0].Delta.Content)
-	}
-}
-
-func sseHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-
-	flusher, ok := w.(http.Flusher)
-	if !ok {
-		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
-		return
-	}
-
-	// Send a new message every second
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			fmt.Fprintf(w, "data: %s\n\n", time.Now().Format("15:04:05"))
-			flusher.Flush()
-		case <-r.Context().Done():
-			return
-		}
 	}
 }
