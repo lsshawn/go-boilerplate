6a22ebcff5a8c3737a264dc2320e99101d23ccbf
sshawn <github.mlxup@simplelogin.com>
2024-11-25 00:06:22 +0800

    fix: remove handlers

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"context"
 	"log"
 
 	"boilerplate/internal/database"
-	"boilerplate/internal/handlers"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
@@ -28,18 +26,7 @@ func main() {
 		return c.String(200, "pong")
 	})
 
-	app.GET("/", func(c echo.Context) error {
-		component := views.Index()
-		return component.Render(context.Background(), c.Response().Writer)
-	})
-
-	app.Static("/css", "css")
 	app.Static("/static", "static")
-
-	app.GET("/account", handlers.AccountPage)
-	app.POST("/account/request-otp", handlers.RequestOTP)
-	app.POST("/account/validate-otp", handlers.ValidateOTP)
-	app.POST("/account/logout", handlers.Logout)
 
 	app.Logger.Fatal(app.Start(":1323"))
 }
